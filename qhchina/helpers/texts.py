d23045a29f438ce3b7673a55a2176b7571ecd12c
Maciej Kurzynski <makurz@gmail.com>
2025-02-26 16:33:41 +0800

    adding new functions for loading text files

---
+++ qhchina/helpers/texts.py
@@ -0,0 +1,21 @@
+def load_text(filenames):
+    """
+    Loads text from a file or a list of files.
+
+    Parameters:
+    filenames (str or list): The filename or list of filenames to load text from.
+
+    Returns:
+    str or list: The text content of the file or a list of text contents if multiple files are provided.
+    """
+    if isinstance(filenames, str):
+        with open(filenames, 'r', encoding='utf-8') as file:
+            return file.read()
+    elif isinstance(filenames, list):
+        texts = []
+        for filename in filenames:
+            with open(filename, 'r', encoding='utf-8') as file:
+                texts.append(file.read())
+        return texts
+    else:
+        raise ValueError("filenames must be a string or a list of strings")
