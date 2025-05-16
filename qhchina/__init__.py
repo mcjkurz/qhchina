ba1dd0fea0f0e927801d83e87db7a90daa4af918
Maciej Kurzynski <makurz@gmail.com>
2025-05-17 00:33:20 +0800

    added matplotlib requirement

--- qhchina/__init__.py
+++ qhchina/__init__.py
@@ -8,7 +8,7 @@ For more specialized functions, import from specific modules:
 - qhchina.educational: Educational visualization tools
 """
 
-__version__ = "0.0.48"
+__version__ = "0.0.49"
 
 # Import helper functions directly into the package namespace
 from .helpers.fonts import load_fonts, current_font, set_font
