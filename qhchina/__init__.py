15a6a470bd1a945aae3cdee1b4d1a21ded66a586
Maciej Kurzynski <makurz@gmail.com>
2025-10-11 11:12:44 +0800

    plot collocations; corrected documentation

--- qhchina/__init__.py
+++ qhchina/__init__.py
@@ -8,7 +8,7 @@ For more specialized functions, import from specific modules:
 - qhchina.educational: Educational visualization tools
 """
 
-__version__ = "0.0.54"
+__version__ = "0.0.55"
 
 # Import helper functions directly into the package namespace
 from .helpers.fonts import load_fonts, current_font, set_font, list_available_fonts, list_font_aliases
